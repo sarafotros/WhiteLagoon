d3070ce12e706a669c8858417b435bb5a3c1c4f4
Sara Fotros <sarafotros@gmail.com>
2024-07-09 23:04:51 +0100

    entitiy villa added

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,14 @@
+namespace ClassLibrary1.Entities;
+
+public class Villa
+{
+    public int Id { get; set; }
+    public required string Name{ get; set; }
+    public string? Description { get; set; }
+    public int Price { get; set; }
+    public int Sqft { get; set; }
+    public int Occupancy { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
